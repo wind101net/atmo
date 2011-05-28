ef0f066eaf7d3435b4330c5597a6bbdfac804ccf
aarondandy <aaron.dandy@gmail.com>
2011-05-27 23:45:52 -0400

    Added the DevEx UI

---
+++ src/Atmo.UI.DevEx/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿// ================================================================================
+//
+// Atmo 2
+// Copyright (C) 2011  BARANI DESIGN
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// 
+// Contact: Jan Barani mailto:jan@baranidesign.com
+//
+// ================================================================================
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Atmo.UI.DevEx")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a8e4f7a7-0496-43e9-a11a-eb3340a90f1c")]
