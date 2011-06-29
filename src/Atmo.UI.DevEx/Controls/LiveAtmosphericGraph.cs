2699fecd87322a5990be2242fa215c75737af8d5
aarondandy <aaron.dandy@gmail.com>
2011-06-29 07:06:28 -0400

    Break the UI into user controls.

---
+++ src/Atmo.UI.DevEx/Controls/LiveAtmosphericGraph.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Atmo.UI.DevEx.Controls {
+	public partial class LiveAtmosphericGraph : DevExpress.XtraEditors.XtraUserControl {
+		public LiveAtmosphericGraph() {
+			InitializeComponent();
+		}
+	}
+}
