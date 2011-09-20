9e9581e40ea106fce32f52aa03fd1032e91dcd7e
aarondandy <aaron.dandy@gmail.com>
2011-09-19 22:00:24 -0400

    added 10 min with stdv and fix for column headers. closes gh-5

--- src/Atmo.UI.DevEx/ExportForm.Designer.cs
+++ src/Atmo.UI.DevEx/ExportForm.Designer.cs
@@ -31,22 +31,26 @@
 			this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
 			this.btnExecute = new DevExpress.XtraEditors.SimpleButton();
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.groupControl3 = new DevExpress.XtraEditors.GroupControl();
+			this.comboBoxEditExportType = new DevExpress.XtraEditors.ComboBoxEdit();
 			((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
 			this.groupControl1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.comboChooseSensor.Properties)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.groupControl2)).BeginInit();
 			this.groupControl2.SuspendLayout();
 			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.groupControl3)).BeginInit();
+			this.groupControl3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.comboBoxEditExportType.Properties)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// groupControl1
 			// 
-			this.groupControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
 			this.groupControl1.Controls.Add(this.comboChooseSensor);
-			this.groupControl1.Location = new System.Drawing.Point(12, 12);
+			this.groupControl1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.groupControl1.Location = new System.Drawing.Point(0, 0);
 			this.groupControl1.Name = "groupControl1";
-			this.groupControl1.Size = new System.Drawing.Size(291, 45);
+			this.groupControl1.Size = new System.Drawing.Size(377, 45);
 			this.groupControl1.TabIndex = 0;
 			this.groupControl1.Text = "1. Select Sensor Database";
 			// 
@@ -58,18 +62,16 @@
 			this.comboChooseSensor.Properties.AllowNullInput = DevExpress.Utils.DefaultBoolean.False;
 			this.comboChooseSensor.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-			this.comboChooseSensor.Size = new System.Drawing.Size(287, 20);
+			this.comboChooseSensor.Size = new System.Drawing.Size(373, 20);
 			this.comboChooseSensor.TabIndex = 0;
 			// 
 			// groupControl2
 			// 
-			this.groupControl2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-						| System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
 			this.groupControl2.Controls.Add(this.dateTimeRangePicker);
-			this.groupControl2.Location = new System.Drawing.Point(12, 63);
+			this.groupControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.groupControl2.Location = new System.Drawing.Point(0, 45);
 			this.groupControl2.Name = "groupControl2";
-			this.groupControl2.Size = new System.Drawing.Size(291, 88);
+			this.groupControl2.Size = new System.Drawing.Size(377, 85);
 			this.groupControl2.TabIndex = 1;
 			this.groupControl2.Text = "2. Select Date/Time Range";
 			// 
@@ -79,7 +81,7 @@
 			this.dateTimeRangePicker.From = new System.DateTime(((long)(0)));
 			this.dateTimeRangePicker.Location = new System.Drawing.Point(2, 22);
 			this.dateTimeRangePicker.Name = "dateTimeRangePicker";
-			this.dateTimeRangePicker.Size = new System.Drawing.Size(287, 64);
+			this.dateTimeRangePicker.Size = new System.Drawing.Size(373, 61);
 			this.dateTimeRangePicker.TabIndex = 0;
 			this.dateTimeRangePicker.To = new System.DateTime(((long)(0)));
 			// 
@@ -88,7 +90,7 @@
 			this.btnCancel.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.btnCancel.Location = new System.Drawing.Point(5, 5);
 			this.btnCancel.Name = "btnCancel";
-			this.btnCancel.Size = new System.Drawing.Size(137, 25);
+			this.btnCancel.Size = new System.Drawing.Size(180, 25);
 			this.btnCancel.TabIndex = 2;
 			this.btnCancel.Text = "Cancel";
 			this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
@@ -96,40 +98,61 @@
 			// btnExecute
 			// 
 			this.btnExecute.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.btnExecute.Location = new System.Drawing.Point(148, 5);
+			this.btnExecute.Location = new System.Drawing.Point(191, 5);
 			this.btnExecute.Name = "btnExecute";
-			this.btnExecute.Size = new System.Drawing.Size(138, 25);
+			this.btnExecute.Size = new System.Drawing.Size(181, 25);
 			this.btnExecute.TabIndex = 3;
 			this.btnExecute.Text = "Export";
 			this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
 			// 
 			// tableLayoutPanel1
 			// 
-			this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
 			this.tableLayoutPanel1.ColumnCount = 2;
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
 			this.tableLayoutPanel1.Controls.Add(this.btnExecute, 1, 0);
 			this.tableLayoutPanel1.Controls.Add(this.btnCancel, 0, 0);
-			this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 157);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 176);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
 			this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(2);
 			this.tableLayoutPanel1.RowCount = 1;
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(291, 35);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(377, 35);
 			this.tableLayoutPanel1.TabIndex = 4;
+			// 
+			// groupControl3
+			// 
+			this.groupControl3.Controls.Add(this.comboBoxEditExportType);
+			this.groupControl3.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.groupControl3.Location = new System.Drawing.Point(0, 130);
+			this.groupControl3.Name = "groupControl3";
+			this.groupControl3.Size = new System.Drawing.Size(377, 46);
+			this.groupControl3.TabIndex = 5;
+			this.groupControl3.Text = "3. Export Type";
+			// 
+			// comboBoxEditExportType
+			// 
+			this.comboBoxEditExportType.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.comboBoxEditExportType.Location = new System.Drawing.Point(2, 22);
+			this.comboBoxEditExportType.Name = "comboBoxEditExportType";
+			this.comboBoxEditExportType.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+			this.comboBoxEditExportType.Properties.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.DisableTextEditor;
+			this.comboBoxEditExportType.Size = new System.Drawing.Size(373, 20);
+			this.comboBoxEditExportType.TabIndex = 0;
 			// 
 			// ExportForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(315, 204);
-			this.Controls.Add(this.tableLayoutPanel1);
+			this.ClientSize = new System.Drawing.Size(377, 211);
 			this.Controls.Add(this.groupControl2);
+			this.Controls.Add(this.groupControl3);
+			this.Controls.Add(this.tableLayoutPanel1);
 			this.Controls.Add(this.groupControl1);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.MaximumSize = new System.Drawing.Size(1024, 242);
+			this.MaximumSize = new System.Drawing.Size(1024, 300);
 			this.MinimumSize = new System.Drawing.Size(16, 242);
 			this.Name = "ExportForm";
 			this.Text = "ExportForm";
@@ -140,6 +163,9 @@
 			((System.ComponentModel.ISupportInitialize)(this.groupControl2)).EndInit();
 			this.groupControl2.ResumeLayout(false);
 			this.tableLayoutPanel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.groupControl3)).EndInit();
+			this.groupControl3.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.comboBoxEditExportType.Properties)).EndInit();
 			this.ResumeLayout(false);
 
         }
@@ -153,5 +179,7 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private DevExpress.XtraEditors.ComboBoxEdit comboChooseSensor;
         private Controls.DateTimeRangePicker dateTimeRangePicker;
+		private DevExpress.XtraEditors.GroupControl groupControl3;
+		private DevExpress.XtraEditors.ComboBoxEdit comboBoxEditExportType;
     }
 }
--- src/Atmo.UI.DevEx/ExportForm.cs
+++ src/Atmo.UI.DevEx/ExportForm.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows.Forms;
 using Atmo.Data;
 using System.Linq;
@@ -8,7 +7,12 @@ using System.IO;
 namespace Atmo.UI.DevEx {
     public partial class ExportForm : DevExpress.XtraEditors.XtraForm {
 
-        private IDataStore _dataStore;
+		private enum ExportType {
+			TenMinute = 0,
+			Raw,
+		}
+
+        private readonly IDataStore _dataStore;
 
         public ExportForm(IDataStore dataStore) {
             _dataStore = dataStore;
@@ -23,6 +27,10 @@ namespace Atmo.UI.DevEx {
             var dbSensorNamesArray = dbSensorInfos.Select(sim => sim.Name).ToArray();
             comboChooseSensor.Properties.Items.Clear();
             comboChooseSensor.Properties.Items.AddRange(dbSensorNamesArray);
+			comboBoxEditExportType.Properties.Items.Clear();
+			comboBoxEditExportType.Properties.Items.AddRange(
+				Enum.GetValues(typeof(ExportType)));
+        	comboBoxEditExportType.SelectedItem = default(ExportType);
         }
 
         private void btnCancel_Click(object sender, EventArgs e) {
@@ -49,7 +57,108 @@ namespace Atmo.UI.DevEx {
 			writer.Write('\"');
 		}
 
-        private bool ExecuteExport(string csvFileName) {
+		private bool ExecuteExport(string csvFileName) {
+			switch((ExportType)comboBoxEditExportType.SelectedItem)
+			{
+			case ExportType.Raw:
+				return ExecuteExportRaw(csvFileName);
+			case ExportType.TenMinute:
+				return ExecuteExportTenMinute(csvFileName);
+			default:
+				return false;
+			}
+		}
+
+		private bool ExecuteExportTenMinute(string csvFileName) {
+			csvFileName = Path.ChangeExtension(csvFileName, "csv");
+			var iniFileName = Path.ChangeExtension(csvFileName, "ini");
+			using (var iniStream = File.Open(iniFileName, FileMode.Create, FileAccess.ReadWrite)) {
+				using (var iniWriter = new StreamWriter(iniStream)) {
+					iniWriter.Write('[');
+					iniWriter.Write(Path.GetFileName(csvFileName));
+					iniWriter.WriteLine(']');
+					iniWriter.WriteLine("ColnameHeader=True");
+					iniWriter.WriteLine("Format=CSVDelimited");
+					iniWriter.WriteLine("DateTimeFormat=yyyy/MM/dd HH:nn:ss");
+					iniWriter.WriteLine("Col1=Stamp DateTime");
+					iniWriter.WriteLine("Col2=Temperature Double");
+					iniWriter.WriteLine("Col3=TempSTDV Double");
+					iniWriter.WriteLine("Col4=Pressure Double");
+					iniWriter.WriteLine("Col5=PresSTDV Double");
+					iniWriter.WriteLine("Col6=Humidity Double");
+					iniWriter.WriteLine("Col7=HumiSTDV Double");
+					iniWriter.WriteLine("Col8=Speed Double");
+					iniWriter.WriteLine("Col9=SpdSTDV Double");
+					iniWriter.WriteLine("Col10=Direction Double");
+					iniWriter.WriteLine("Col11=DirSTDV Double");
+					iniWriter.Flush();
+				}
+			}
+			var readings = _dataStore.GetReadingSummaries(
+				comboChooseSensor.SelectedText,
+				dateTimeRangePicker.Min,
+				dateTimeRangePicker.Max.Subtract(dateTimeRangePicker.Min),
+				new TimeSpan(0,0,10,0)
+			);
+			int c = 0;
+			using (var csvStream = File.Open(csvFileName, FileMode.Create, FileAccess.ReadWrite)) {
+				using (var csvWriter = new StreamWriter(csvStream)) {
+
+					WriteQuoted(csvWriter, "Stamp");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Temperature");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Temperature (stdv)");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Pressure");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Pressure (stdv)");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Humidity");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Humidity (stdv)");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Speed");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Speed (stdv)");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Direction");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Direction (stdv)");
+					csvWriter.WriteLine();
+
+					foreach (var reading in readings) {
+						c++;
+						WriteQuoted(csvWriter, reading.BeginStamp.ToString("yyyy/MM/dd HH:mm:ss"));
+						csvWriter.Write(',');
+						WriteQuoted(csvWriter, reading.Mean.Temperature.ToString());
+						csvWriter.Write(',');
+						WriteQuoted(csvWriter, reading.SampleStandardDeviation.Temperature.ToString());
+						csvWriter.Write(',');
+						WriteQuoted(csvWriter, reading.Mean.Pressure.ToString());
+						csvWriter.Write(',');
+						WriteQuoted(csvWriter, reading.SampleStandardDeviation.Pressure.ToString());
+						csvWriter.Write(',');
+						WriteQuoted(csvWriter, reading.Mean.Humidity.ToString());
+						csvWriter.Write(',');
+						WriteQuoted(csvWriter, reading.SampleStandardDeviation.Humidity.ToString());
+						csvWriter.Write(',');
+						WriteQuoted(csvWriter, reading.Mean.WindSpeed.ToString());
+						csvWriter.Write(',');
+						WriteQuoted(csvWriter, reading.SampleStandardDeviation.WindSpeed.ToString());
+						csvWriter.Write(',');
+						WriteQuoted(csvWriter, reading.Mean.WindDirection.ToString());
+						csvWriter.Write(',');
+						WriteQuoted(csvWriter, reading.SampleStandardDeviation.WindDirection.ToString());
+						csvWriter.WriteLine();
+					}
+					csvWriter.Flush();
+				}
+			}
+			return c > 0;
+		}
+
+    	private bool ExecuteExportRaw(string csvFileName) {
             csvFileName = Path.ChangeExtension(csvFileName, "csv");
             var iniFileName = Path.ChangeExtension(csvFileName, "ini");
             using (var iniStream = File.Open(iniFileName, FileMode.Create, FileAccess.ReadWrite)) {
@@ -77,6 +186,20 @@ namespace Atmo.UI.DevEx {
             int c = 0;
             using (var csvStream = File.Open(csvFileName, FileMode.Create, FileAccess.ReadWrite)) {
                 using (var csvWriter = new StreamWriter(csvStream)) {
+
+					WriteQuoted(csvWriter, "Stamp");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Temperature");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Pressure");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Humidity");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Speed");
+					csvWriter.Write(',');
+					WriteQuoted(csvWriter, "Direction");
+					csvWriter.WriteLine();
+
                     foreach (var reading in readings) {
                         c++;
 						WriteQuoted(csvWriter,reading.TimeStamp.ToString("yyyy/MM/dd HH:mm:ss"));
